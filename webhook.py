e0ad9c22a2bf4a66d969b845a34dc0436239885e
Egor <paradize9140@gmail.com>
2025-06-14 10:16:23 +0200

    first commit

--- webhook.py
+++ webhook.py
@@ -2,17 +2,22 @@ import logging
 from fastapi import FastAPI, Request
 from aiogram.types import Update
 from bot import dp
-from config import bot # ✅ заменил импорт
+from config import bot
+
 logging.basicConfig(level=logging.INFO)
+
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
-    await bot.set_webhook("https://botiiko-production.railway.app/webhook")
+    logging.info("🚀 Устанавливаю webhook...")
+    await bot.set_webhook("https://botiiko-production.up.railway.app/webhook")
 
 @app.post("/webhook")
 async def webhook(request: Request):
+    logging.info("📥 Webhook получил запрос")
     data = await request.json()
+    logging.debug(f"Payload: {data}")
     update = Update.model_validate(data)
     await dp.feed_update(bot, update)
     return {"ok": True}

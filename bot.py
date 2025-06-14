980af81f2803accefb9168a60ed202eec489903e
Egor <paradize9140@gmail.com>
2025-06-14 09:51:12 +0200

    first commit

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
 
-from handlers import basic_commands, salary, production  # твои роутеры
+from handlers import commands # твои роутеры
 
 # Загрузка токена из переменной окружения
 TOKEN = os.getenv("BOT_TOKEN")
@@ -16,6 +16,5 @@ bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
 
 # Регистрация роутеров
-dp.include_router(basic_commands.router)
-dp.include_router(salary.router)
-dp.include_router(production.router)
+dp.include_router(commands.router)
+
